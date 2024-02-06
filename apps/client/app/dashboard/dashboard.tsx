dedc611dc69e19bbbed1443090fc99e835f11d58
Manel Soler <manelsoler@gmail.com>
2024-02-06 12:07:37 +0100

    create dashbord page

---
+++ apps/client/app/dashboard/dashboard.tsx
@@ -0,0 +1,19 @@
+import react from 'react'
+
+function Dashboard (){
+
+  return (
+    
+    <div>
+      <div>
+        <button> Start Battle </button>
+        <div><h3> Join a Battle </h3>
+        <div> The table here</div>
+        </div>
+      </div>
+    </div>
+  )
+
+}
+
+export default Dashboard
