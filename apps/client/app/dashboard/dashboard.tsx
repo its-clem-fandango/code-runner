2d037bb9a4e7e12a3becf099b77ee8163189bc18
Manel Soler <manelsoler@gmail.com>
2024-02-06 15:37:24 +0100

    added table to the dashboard

--- apps/client/app/dashboard/dashboard.tsx
+++ apps/client/app/dashboard/dashboard.tsx
@@ -1,9 +1,44 @@
 "use client"
 
 import { Button } from "@/components/ui/button";
+import { useState, useEffect } from 'react'
+import { Payment, columns } from '@/app/dataTable/columns'
+import {DataTable} from '@/app/dataTable/data-table'
 
-function Dashboard (){
 
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  
+  const data: Payment[] = [
+    
+      {
+        id: "728ed52f",
+        amount: 100,
+        status: "pending",
+        email: "m@example.com",
+      },
+      // ...
+      
+      
+    ]
+    return data
+}
+
+ function Dashboard() {
+
+  
+  const [battleList,setBattleList] = useState<Payment[]>([])
+
+   useEffect(() => {
+     const fetchData = async () => {
+       const data = await getData()
+       setBattleList(data)
+     }
+
+     fetchData()
+   }, [])
+   
+   
   function handleNewGame(e: React.MouseEvent<HTMLButtonElement>) {
     e.preventDefault()
     console.log("Button Clicked")
@@ -13,11 +48,17 @@ function Dashboard (){
     
     <div>
       <div>
-      <Button onClick={handleNewGame}>New Game</Button>
-        <button> Start Battle </button>
-        <div><h3> Join a Battle </h3>
-        <div> The table here</div>
+      <Button onClick={handleNewGame}>Start Battle</Button>
         </div>
+      
+      <div>
+        {/* rendert a table with 4 columns ( battle name, username, level of challenge, jopin button) */}
+
+        
+        <h3> Join a Battle </h3>
+             <DataTable columns={columns} data={battleList} />
+
+         <div> The table here</div>
       </div>
     </div>
   )
