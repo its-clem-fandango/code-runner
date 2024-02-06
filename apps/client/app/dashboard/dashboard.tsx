e7b0efbe03d5115e7a93db8e598483e91231a295
Manel Soler <manelsoler@gmail.com>
2024-02-06 14:25:45 +0100

    fix html problem and changing title page

--- apps/client/app/dashboard/dashboard.tsx
+++ apps/client/app/dashboard/dashboard.tsx
@@ -1,13 +1,19 @@
-import react from 'react'
+"use client"
+
 import { Button } from "@/components/ui/button";
 
 function Dashboard (){
+
+  function handleNewGame(e: React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault()
+    console.log("Button Clicked")
+  }
 
   return (
     
     <div>
       <div>
-      <Button>This is my ShadCN Button</Button>
+      <Button onClick={handleNewGame}>New Game</Button>
         <button> Start Battle </button>
         <div><h3> Join a Battle </h3>
         <div> The table here</div>
--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,13 +1,9 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
 import Dashboard from "./dashboard/dashboard";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
-      
-      {Dashboard()}
+      <Dashboard />
     </main>
   );
 }
