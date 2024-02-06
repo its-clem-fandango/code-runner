bdc7bb16cf3bf710f28f03181ef9dda0afcb007e
Ansh <ansh.bindal1996@gmail.com>
2024-02-06 19:27:33 +0530

    Trying Socket IO in NestJs - Incomplete

---
+++ apps/server/src/modules/socket-code-editor/socket-code-editor.service.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { SocketCodeEditorService } from './socket-code-editor.service';
+
+describe('SocketCodeEditorService', () => {
+  let service: SocketCodeEditorService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [SocketCodeEditorService],
+    }).compile();
+
+    service = module.get<SocketCodeEditorService>(SocketCodeEditorService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
