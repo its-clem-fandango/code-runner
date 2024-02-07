80100ae157a54bb84d7fa176d4c20d9c01a16781
E1NS01 <jl_fischer@icloud.com>
2024-02-07 16:52:19 +0100

    Player number fixed

--- apps/client/components/ui/MonacoCodeEditor.tsx
+++ apps/client/components/ui/MonacoCodeEditor.tsx
@@ -16,6 +16,7 @@ export default function MonacoCodeEditor() {
   const [fileName, setFileName] = useState<string>('script.js')
   const [code, setCode] = useState<string | undefined>('')
   const [recievedCode, setRecievedCode] = useState<string>('')
+  const [playerNumber, setPlayerNumber] = useState<number>(1)
 
   const roomName = 1
   const files = {
@@ -34,17 +35,21 @@ export default function MonacoCodeEditor() {
     // check later for undefined code
     //if (!code) return
     console.log(`EMIT MSG:`, code)
-    socket.emit('codeChanged', { room: roomName, player: 1, message: code })
+    socket.emit('codeChanged', {
+      room: roomName,
+      player: playerNumber,
+      message: code,
+    })
   }
   useEffect(() => {
     socket.on('opponentCode', (msg) => {
-      if (msg.player !== 1) {
+      if (msg.player !== playerNumber) {
         console.log(`This is the message: ${msg.message}`)
         setRecievedCode(msg.message)
         console.log(`This is recieved: ${recievedCode}`)
       }
     })
-  }, [])
+  }, [playerNumber])
   socket.emit('join room', roomName)
 
   return (
@@ -57,6 +62,14 @@ export default function MonacoCodeEditor() {
         >
           script.js
         </button>
+        <button
+          onClick={() =>
+            playerNumber === 1 ? setPlayerNumber(2) : setPlayerNumber(1)
+          }
+        >
+          Change Player
+        </button>
+        <p>{playerNumber}</p>
         <div className='editors-container'>
           <Editor
             height='60vh'
