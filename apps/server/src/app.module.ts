74603e006e3691ebdfc23ab001b8244b3b296a6b
E1NS01 <jl_fischer@icloud.com>
2024-02-09 11:39:43 +0100

    Fix: Websocket Gateway structure unified

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AnswerModule } from './modules/answer/answer.module';
-import { GatewayModule } from './gateway/gateway.module';
+import { GatewayModule } from './gateways/gateway.module';
 import { BattleModule } from './modules/battles/battle.module';
 
 @Module({
