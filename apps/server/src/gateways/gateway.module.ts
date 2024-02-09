461046dc9f246ad00e15f214e38a77fef7c780b6
E1NS01 <jl_fischer@icloud.com>
2024-02-09 11:44:24 +0100

    Changed battles gateway name/import

--- apps/server/src/gateways/gateway.module.ts
+++ apps/server/src/gateways/gateway.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { EditorGateway } from './editor/editor.gateway';
 import { AnswerService } from 'src/modules/answer/answer.service';
-import { BattleGateway } from './battles/battlesGateway';
+import { BattleGateway } from './battles/battles.gateway';
 import { BattleService } from 'src/modules/battles/battle.service';
 
 @Module({
