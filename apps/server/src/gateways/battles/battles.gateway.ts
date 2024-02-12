60b6ae38c3fdb73412deec24cda63ee8638eef60
Manel Soler <manelsoler@gmail.com>
2024-02-12 21:13:03 +0100

    feat(server): create a controler service and module for randomnly  serve a challengeCode

--- apps/server/src/gateways/battles/battles.gateway.ts
+++ apps/server/src/gateways/battles/battles.gateway.ts
@@ -5,23 +5,23 @@ import {
   WebSocketServer,
   OnGatewayConnection,
   ConnectedSocket,
-} from '@nestjs/websockets';
-import { Server } from 'socket.io';
-import { BattleService } from 'src/modules/battles/battle.service';
+} from '@nestjs/websockets'
+import { Server } from 'socket.io'
+import { BattleService } from 'src/modules/battles/battle.service'
 
 @WebSocketGateway(8082, { cors: true })
 export class BattleGateway implements OnGatewayConnection {
   constructor(private readonly battleService: BattleService) {}
-  @WebSocketServer() server: Server;
+  @WebSocketServer() server: Server
 
   handleConnection(client: any) {
-    this.sendBattles(client);
-    return;
+    this.sendBattles(client)
+    return
   }
   private async sendBattles(client: any) {
-    const battles = await this.battleService.getBattles();
-    client.emit('availableBattles', battles);
-    return;
+    const battles = await this.battleService.getBattles()
+    client.emit('availableBattles', battles)
+    return
   }
 
   @SubscribeMessage('createBattle')
@@ -32,26 +32,26 @@ export class BattleGateway implements OnGatewayConnection {
       data.battleName,
       'Test',
       data.difficulty,
-      'random@email.com',
-    );
-    this.server.emit('availableBattles', battles);
-    return;
+      'random-challenge',
+    )
+    this.server.emit('availableBattles', battles)
+    return
   }
   @SubscribeMessage('joinBattle')
   async joinBattle(
     @MessageBody() data: { id: number },
     @ConnectedSocket() client: any,
   ) {
-    const battleInfo = this.battleService.getBattle(data.id);
+    const battleInfo = this.battleService.getBattle(data.id)
     if (battleInfo.playerCount >= 2) {
-      this.server.emit('battleError', { full: true });
-      return;
+      this.server.emit('battleError', { full: true })
+      return
     } else {
-      this.server.socketsJoin(`room${data.id}`);
-      const joinedBattle = this.battleService.updateBattle(data.id);
+      this.server.socketsJoin(`room${data.id}`)
+      const joinedBattle = this.battleService.updateBattle(data.id)
       this.server
         .to(`room${data.id}`)
-        .emit('joinedBattle', { ...joinedBattle, clientId: client.id });
+        .emit('joinedBattle', { ...joinedBattle, clientId: client.id })
     }
   }
 }
--- apps/server/src/modules/battles/battle.module.ts
+++ apps/server/src/modules/battles/battle.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
-import { BattleService } from './battle.service';
-import { BattleController } from './battle.controller';
+import { Module } from '@nestjs/common'
+import { BattleService } from './battle.service'
+import { BattleController } from './battle.controller'
+import { CodingChallengesModule } from '../coding-challenges/coding-challenges.module'
 
 @Module({
+  imports: [CodingChallengesModule],
   providers: [BattleService],
   controllers: [BattleController],
 })
