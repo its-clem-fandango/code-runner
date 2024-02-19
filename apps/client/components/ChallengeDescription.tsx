b079a8d33e2ae6911e99c1ccfcc6de6661de9617
Arol Viñolas <arol@arol.dev>
2024-02-19 13:05:41 +0100

    fix(frontend): fixed some battle styles

--- apps/client/components/ChallengeDescription.tsx
+++ apps/client/components/ChallengeDescription.tsx
@@ -15,10 +15,10 @@ function ChallengeDescription() {
   }, [race?.challenge])
 
   return (
-    <div className="w-[50%] h-[90vh] bg-white rounded-lg shadow" >
+    <div className="w-[50%] h-[90vh] bg-white rounded-lg shadow">
       {/* battle info */}
-      <div className="flex justify-between mr-5">
-        <h1 className="text-2xl font-bold p-4">
+      <div className="flex justify-between mt-5 mr-5">
+        <h1 className="text-2xl font-bold p-4 px-8">
           {race ? race.battleName : null}
         </h1>
         {race?.challenge?.difficultyOfChallenge === "easy" ? (
@@ -48,25 +48,27 @@ function ChallengeDescription() {
       </div>
       {/* Challenge info */}
       <div>
-        <div className="w-[45vw]" >
-          <div className="flex justify-between mr-5">
-            <h1 className="font-bold text-xl border p-4 tracking-wide">
+        <div className="w-[45vw]">
+          <div className="flex justify-between mr-5 px-4">
+            <h1 className="font-bold text-xl p-4 tracking-wide">
               {race?.challenge?.name}
             </h1>
           </div>
 
-          <ScrollArea className="h-[200px] w-[45vw] p-4 tracking-wide">
+          <ScrollArea className="h-[200px] p-4 px-8">
             <ReactMarkdown>{description}</ReactMarkdown>
           </ScrollArea>
-          <div className="bg-grey-200 border w-[45vw]">
-            <Tabs defaultValue="example" className="w-[40vw] p-4 ">
-              <TabsList className=" w-[41vw] flex justify-start bg-[#F6F6F6]">
+          <div className="bg-grey-200 w-full">
+            <Tabs defaultValue="example" className="w-full p-4">
+              <TabsList className=" w-full flex justify-start bg-[#F6F6F6]">
                 <TabsTrigger value="example">Example</TabsTrigger>
                 <TabsTrigger value="console">Outcome</TabsTrigger>
               </TabsList>
-              <TabsContent value="example" className=" py-2">
+              <TabsContent value="example" className="px-4 py-2">
                 {/* <h1 className="text-xl font-bold py-2">Example</h1> */}
-                <p>{race?.challenge?.example ? race?.challenge.example : null}</p>
+                <p>
+                  {race?.challenge?.example ? race?.challenge.example : null}
+                </p>
               </TabsContent>
               <TabsContent value="console" className=" py-2">
                 <ChallengeConsole
