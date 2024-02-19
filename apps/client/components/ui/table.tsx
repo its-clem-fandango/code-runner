6d700cf7cb8b3e5df5123411b72dcdd49ca22606
Arol Viñolas <arol@arol.dev>
2024-02-19 12:35:43 +0100

    fix(frontend): dashboard table responsive from md to xl

--- apps/client/components/ui/table.tsx
+++ apps/client/components/ui/table.tsx
@@ -6,7 +6,7 @@ const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
 >(({ className, ...props }, ref) => (
-  <div className="rounded-lg w-[791px] bg-[#FAFAFA]">
+  <div className="rounded-lg bg-[#FAFAFA]">
     <table
       ref={ref}
       className={cn("w-full caption-bottom text-sm", className)}
