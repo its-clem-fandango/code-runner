80388ab749275532f6189978e6a649a13b664b9f
Arol Viñolas <arol@arol.dev>
2024-02-19 13:32:08 +0100

    feat(frontend): header

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,14 +1,38 @@
 "use client"
 
+import { cn } from "@/lib/utils"
 import Dashboard from "./dashboard/dashboard"
 import { RacesCollectionProvider } from "@/lib/useRacesCollection"
+import CodeRacerLogo from "@/public/code-racer-logo.png"
+import { Rowdies } from "next/font/google"
+import Image from "next/image"
+
+const rowdies = Rowdies({
+  weight: ["400"],
+  subsets: ["latin"],
+})
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <RacesCollectionProvider>
-        <Dashboard />
-      </RacesCollectionProvider>
-    </main>
+    <>
+      <div className="h-20 bg-slate-900 py-4 px-24 flex items-center text-white gap-4">
+        <div className="bg-white rounded-full w-[40px] h-[40px] flex items-center justify-center">
+          <Image
+            src={CodeRacerLogo}
+            width={24}
+            height={24}
+            alt="CodeRacer's logo"
+          />
+        </div>
+        <span className={cn(rowdies.className, "uppercase text-2xl")}>
+          Code Racer
+        </span>
+      </div>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <RacesCollectionProvider>
+          <Dashboard />
+        </RacesCollectionProvider>
+      </main>
+    </>
   )
 }
