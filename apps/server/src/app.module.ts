bda33de2d76c129a6b386fe6c8e7a071aeb2fd32
Clem Fandango <drosshere@gmail.com>
2024-03-13 16:26:11 +0100

    Add cookie-parser and middleware for Auth

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,17 +1,22 @@
-import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
-import { AnswerModule } from './modules/answer/answer.module'
-import { GatewayModule } from './gateways/gateway.module'
-import { BattleModule } from './modules/battles/battle.module'
-import { CodingChallengesModule } from './modules/coding-challenges/coding-challenges.module'
-import { AuthController } from './auth/auth.controller';
-import { UsersService } from './users/users.service';
-import { UsersController } from './users/users.controller';
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { AnswerModule } from "./modules/answer/answer.module";
+import { GatewayModule } from "./gateways/gateway.module";
+import { BattleModule } from "./modules/battles/battle.module";
+import { CodingChallengesModule } from "./modules/coding-challenges/coding-challenges.module";
+import { AuthController } from "./auth/auth.controller";
+import { UsersService } from "./users/users.service";
+import { UsersController } from "./users/users.controller";
+import { AuthMiddleware } from "./auth/auth.middleware";
 
 @Module({
   imports: [AnswerModule, GatewayModule, BattleModule, CodingChallengesModule],
   controllers: [AppController, AuthController, UsersController],
   providers: [AppService, UsersService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes("*");
+  }
+}
--- apps/server/src/auth/auth.middleware.ts
+++ apps/server/src/auth/auth.middleware.ts
@@ -1,8 +1,39 @@
 import { Injectable, NestMiddleware } from "@nestjs/common";
+import { NextFunction } from "express";
 
+//checks for presence of accesstoken and validates it
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
-  use(req: any, res: any, next: () => void) {
-    next();
+  async use(req: any, res: any, next: NextFunction) {
+    const accessToken = req.cookies["accessToken"];
+    const excludedAuthPaths = ["/auth/github", "/"];
+
+    if (excludedAuthPaths.includes(req.path)) {
+      return next();
+    }
+
+    if (!accessToken) {
+      return res.status(401).send({ error: "Access Token is missing" });
+    }
+
+    // if accessToken is correct, request profile info of user associated with access token to GH
+    try {
+      const githubResponse = await fetch("https://api.github.com/user", {
+        headers: { Authorization: `token ${accessToken}` },
+      });
+
+      if (!githubResponse.ok) {
+        return res
+          .status(401)
+          .send({ error: "Invalid or expired access token" });
+      }
+
+      const githubUser = await githubResponse.json();
+      console.log("USER ID:", (req.user = { id: githubUser.id }));
+      next();
+    } catch (error) {
+      console.error(error);
+      return res.status(500).send({ error: "Internal Server Error" });
+    }
   }
 }
