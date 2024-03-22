240fa2ad3141bd58e27e327389eee64d9dd9aac3
Clem Fandango <drosshere@gmail.com>
2024-03-22 15:57:29 +0100

    Remove authenticated user from Home()

--- apps/client/app/layout.tsx
+++ apps/client/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html>
+    <html >
       <body
         className={cn(
           "min-h-screen bg-background antialiased",
