b146f94db2e38b7094c40dc32d5a0cc834f30173
Clem Fandango <drosshere@gmail.com>
2024-03-23 16:12:33 +0100

    Add Login w/ Github Btn

--- apps/client/app/login/page.tsx
+++ apps/client/app/login/page.tsx
@@ -3,13 +3,12 @@ import { GitHubLogoIcon } from "@radix-ui/react-icons"
 
 export default function Login() {
   const redirect = process.env.NEXT_PUBLIC_REDIRECT_URI
-    return (
-      <div>
-        <Button >
-          <GitHubLogoIcon className="mr-2 h-4 w-4"/>
-          <a href={redirect}>Github</a>
-          </Button>     
-           </div>
-    )
-  }
-  
+  return (
+    <div>
+      <Button className="bg-white text-slate-900">
+        <GitHubLogoIcon className="mr-2 h-4 w-4" />
+        <a href={redirect}>Login with Github</a>
+      </Button>
+    </div>
+  )
+}
