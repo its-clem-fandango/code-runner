0c69a058f8dd59f97b144d6ff8b4156acc45f79d
Clem Fandango <drosshere@gmail.com>
2024-03-23 16:12:33 +0100

    Add Login w/ Github Btn

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -7,6 +7,7 @@ import CodeRacerLogo from "@/public/code-racer-logo.png"
 import { Rowdies } from "next/font/google"
 import Image from "next/image"
 import { AuthProvider } from "@/lib/useAuth"
+import Login from "./login/page"
 
 const rowdies = Rowdies({
   weight: ["400"],
@@ -16,18 +17,23 @@ const rowdies = Rowdies({
 export default function Home() {
   return (
     <>
-      <div className="h-20 bg-slate-900 py-4 px-24 flex items-center text-white gap-4">
-        <div className="bg-white rounded-full w-[40px] h-[40px] flex items-center justify-center">
-          <Image
-            src={CodeRacerLogo}
-            width={24}
-            height={24}
-            alt="CodeRacer's logo"
-          />
+      <div className="h-20 bg-slate-900 py-4 px-24 flex items-center justify-between text-white gap-4">
+        <div className="flex items-center gap-4">
+          <div className="bg-white rounded-full w-[40px] h-[40px] flex items-center justify-center">
+            <Image
+              src={CodeRacerLogo}
+              width={24}
+              height={24}
+              alt="CodeRacer's logo"
+            />
+          </div>
+          <span className={cn(rowdies.className, "uppercase text-2xl")}>
+            Code Racer
+          </span>
         </div>
-        <span className={cn(rowdies.className, "uppercase text-2xl")}>
-          Code Racer
-        </span>
+        <div>
+          <Login />
+        </div>
       </div>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <AuthProvider>
