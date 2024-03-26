1a8cbbe66078c32dbd6a3cce4e9731c001aacfaa
Clem Fandango <drosshere@gmail.com>
2024-03-26 19:26:58 +0100

    Add user/profile endpoint to fetch userinfo from DB

--- apps/server/src/users/users.controller.ts
+++ apps/server/src/users/users.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UnauthorizedException } from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { Request } from "express";
 
-@Controller('users')
-export class UsersController {}
+@Controller("user")
+export class UsersController {
+  constructor(private usersService: UsersService) {} //Inject UserService
+
+  @Get("profile")
+  async getUserProfile(@Req() request: Request) {
+    const sessionId = request.cookies["sessionId"];
+    if (!sessionId) {
+      throw new UnauthorizedException("Session ID not found");
+    }
+
+    const user = await this.usersService.findUserBySessionId(sessionId);
+    if (!user) {
+      throw new UnauthorizedException(
+        "User not found for the given session ID",
+      );
+    }
+    return user;
+  }
+}
