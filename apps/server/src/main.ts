bfdfad8df42eb9192b9a875d908d5e5321e27cc0
Clem Fandango <drosshere@gmail.com>
2024-04-01 18:17:43 +0200

    Updated main.ts in server to PUBLIC_CLIENT_URL

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ["http://localhost:3000"],
+    origin: ["NEXT_PUBLIC_CLIENT_URL"],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     credentials: true,
   });
