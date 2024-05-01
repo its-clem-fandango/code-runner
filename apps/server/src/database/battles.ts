e83697dd2351a80bc72759e12f779c4cc57b11b9
Clem Fandango <drosshere@gmail.com>
2024-05-01 20:23:21 +0200

    Add Challenges up to 28

--- apps/server/src/database/battles.ts
+++ apps/server/src/database/battles.ts
@@ -1,3 +1,5 @@
+import { codingChallengesList } from "./codingChallenges";
+
 export interface Battle {
   id: number;
   battleName: string;
@@ -7,6 +9,20 @@ export interface Battle {
   join: string;
   challengeId: number;
 }
+
+export const battles = codingChallengesList.map((challenge, index) => {
+  return {
+    id: index,
+    battleName: challenge.name,
+    players: ["player"],
+    difficulty: challenge.difficultyOfChallenge,
+    playerCount: 0,
+    join: "linkToJoinBattle",
+    challengeId: challenge.challengeId,
+  };
+});
+
+/*
 
 export const battles: Battle[] = [
   {
@@ -36,4 +52,14 @@ export const battles: Battle[] = [
     join: "fightme@example.com",
     challengeId: 8,
   },
+  {
+    id: 3, // Replace "someUniqueId" with a unique identifier for this object
+    battleName: "Return Negative Numbers", // The 'name' from the challenge, used as the battle name
+    players: ["player"], // Array containing the default player names, adjust as needed
+    difficulty: "easy", // The 'difficultyOfChallenge' from the challenge details
+    playerCount: 0, // You can set this to the number of players you expect
+    join: "linkToJoinBattle", // Replace "linkToJoinBattle" with an actual link or identifier for joining
+    challengeId: 11, // The 'challengeId' from the challenge details
+  },
 ];
+ */
